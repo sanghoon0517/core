e400884e9832439e9504058a78bf76a1ba6ca34c
jsh940517 <jsh940517@daum.net>
2021-02-22 23:26:17 +0900

    싱글톤 강의 추가

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -17,16 +17,19 @@ public class AppConfig {
     //애플리케이션 동작 전체를 구성(config)하기 위해, 구현 객체를 생성하고, 연결(주입)할 수 있는 책임을 가지는 별도의 설정 클래스
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository()); //생성자 주입
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImple(memberRepository(), discountPolicy());
     }
 
